c544533953f8f6ce4b3d2524dc030962e4d1cb68
lochana2002 <ehltehelapitiya@gmail.com>
2026-07-24 08:57:53 +0530

    chore: configure backend routes and server

--- server/src/app.js
+++ server/src/app.js
@@ -3,9 +3,36 @@ const cors = require("cors");
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 
+
+const authRoutes = require("./routes/authRoutes");
+const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const sellerRoutes = require("./routes/sellerRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
+
+const adminRoutes = require("./routes/adminRoutes");
+const statsRoutes = require("./routes/statsRoutes");
+
+const aiRoutes = require("./routes/aiRoutes");
+
+
+const { protect } = require("./middleware/authMiddleware");
+
+
 const app = express();
 
-app.use(cors());
+
+
+/* Middleware */
+
+app.use(
+cors({
+origin:"http://localhost:5173",
+credentials:true
+})
+);
+
 
 app.use(express.json());
 
@@ -13,10 +40,109 @@ app.use(cookieParser());
 
 app.use(morgan("dev"));
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Handmade Marketplace API Running"
-    });
+
+
+
+
+/* Routes */
+
+
+app.use(
+"/api/auth",
+authRoutes
+);
+
+
+
+app.use(
+"/api/products",
+productRoutes
+);
+
+
+
+app.use(
+"/api/orders",
+orderRoutes
+);
+
+
+
+app.use(
+"/api/cart",
+cartRoutes
+);
+
+
+
+app.use(
+"/api/seller",
+sellerRoutes
+);
+
+
+
+app.use(
+"/api/reviews",
+reviewRoutes
+);
+
+
+
+app.use(
+"/api/admin",
+adminRoutes
+);
+
+
+
+app.use(
+"/api/stats",
+statsRoutes
+);
+
+
+
+app.use(
+"/api/ai",
+aiRoutes
+);
+
+
+
+
+
+app.get(
+"/api/protected",
+protect,
+(req,res)=>{
+
+res.json({
+
+message:"Protected data",
+
+user:req.user
+
 });
+
+});
+
+
+
+
+
+app.get(
+"/",
+(req,res)=>{
+
+res.json({
+
+message:"Handmade Marketplace API Running"
+
+});
+
+});
+
+
 
 module.exports = app;
--- server/src/server.js
+++ server/src/server.js
@@ -1,9 +1,37 @@
+const mongoose = require("mongoose");
 require("dotenv").config();
+
 
 const app = require("./app");
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+
+mongoose.connect(
+process.env.MONGO_URI
+)
+.then(()=>{
+
+console.log(
+"MongoDB Connected"
+);
+
+
+app.listen(
+5001,
+()=>{
+
+console.log(
+"Server running on port 5001"
+);
+
+});
+
+})
+.catch(error=>{
+
+console.log(
+"Database Error:",
+error
+);
+
 });
