d62c1a27f263904994dbf121f7e31f754e4512de
lochana2002 <ehltehelapitiya@gmail.com>
2026-07-24 13:42:01 +0530

    fix:fixed api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -3,39 +3,29 @@ import axios from "axios";
 
 const api = axios.create({
 
-baseURL:"http://localhost:5001/api"
+    baseURL: "http://localhost:5001/api"
 
 });
 
 
-
 api.interceptors.request.use(
+
 (config)=>{
 
+    const token = localStorage.getItem("token");
 
-const token =
-localStorage.getItem("token");
-
-
-
-console.log(
-"TOKEN:",
-token
-);
+    console.log("TOKEN:", token);
 
 
+    if(token){
 
-if(token){
+        config.headers.Authorization =
+        `Bearer ${token}`;
 
-config.headers.Authorization =
-`Bearer ${token}`;
-
-}
-
+    }
 
 
-return config;
-
+    return config;
 
 });
 
